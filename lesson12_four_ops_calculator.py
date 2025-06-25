21b2d827c69a20c763aa372575451cedb8565e0b
qitsuuu <villoranteqit@gmail.com>
2025-06-25 22:21:17 +0900

    four operation calculator with simplified if statement

--- lesson12_four_ops_calculator.py
+++ lesson12_four_ops_calculator.py
@@ -1,21 +1,20 @@
 num1= input("Enter the first number: ")
-if not num1.isalpha():
-    num1=float(num1)
-else:
+if num1.isalpha():
     num1 = input("Enter the second number: ")
+    #if will not use float(), arithmetic ops will not work on str
+num1=float(num1)
 
 operation = input("Enter operation: ")
 num2 = input("Enter the second number: ")
-if not num2.isalpha():
-    num2=float(num2)
-else:
+if num2.isalpha():
     num2 = input("Enter the second number: ")
+num2=float(num2)
 
 #this calculator doesn't have loop yet
 #therefore only until 2nd try for each input
 
 if operation == "+":
-    print("Value is: ", float(num1) + float(num2))
+    print("Value is: ", num1 + num2)
 elif operation == "-":
     print("Value is: ", num1 - num2)
 elif operation == "*":
