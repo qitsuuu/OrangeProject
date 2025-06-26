0b1df4ed946d042fbc79dc3063bcc8891fbc80ad
qitsuuu <villoranteqit@gmail.com>
2025-06-26 23:34:34 +0900

    while loop in a function with if statements

---
+++ lesson13_while loops.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+def testing(count):
+    i=1
+    while i<=count:
+        if i%2!=0:
+            print("I am ODD number ", i)
+        elif i % 2 == 0:
+            print("I am EVEN number ", i)
+        i +=1
+    print("\nOut of the Loop")
+
+testing(num)
